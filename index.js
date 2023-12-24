a0a91037ed3f8726ad97ac878c3b14e31bd57ef8
Amit Jomy <amitjomy007@gmail.com>
2023-12-24 20:05:59 +0530

    added username sync in local Storage

--- index.js
+++ index.js
@@ -1,45 +1,71 @@
-document.getElementById("add-el").addEventListener("click",  addTask );
+document.getElementById("userName-load-el").addEventListener("click", loadPage);
 
-var rows = JSON.parse(localStorage.getItem('rows')) || [];
-var str = "";
-updateinnerHTML();
-function addEventListenerToAllDeleteButtons(arr){
-    for (let i = 0 ; i< arr.length ; i++){
-        document.getElementById(`delete-${i}th`).addEventListener("click", function DeleteAndUpdate() {
-            arr.splice(i,1);               
-            updateinnerHTML();
-        });
-    }
-}
 
-function addTask() {
+
+function loadPage() {
+    var user = document.getElementById("userName-el").value;
+    document.getElementById("user-page").innerHTML = `
+    <p>Let's be productive ${user}!</p>   
+            
+    <div id="input-div" class = "input-grid">
+        <input id = "task-input-el" class = "Lcolumn column" type="text" placeholder="Enter your Task">
+        <input id = "date-input-el" class = "Mcolumn column" type = "datetime-local">
+        <button id = "add-el" class = "Rcolumn column">Add</button>
+    </div> 
     
-    var task = document.getElementById("task-input-el").value;
-    var date = document.getElementById("date-input-el").value;
-    rows.push([task,date]);
-    renderHTML(rows);
-    document.getElementById("container-El").innerHTML = str;   //these four lines maybe replaced
-    addEventListenerToAllDeleteButtons(rows);                  // with updateinnerHTML() function.
-    localStorage.setItem('rows', JSON.stringify(rows))
-}
+    <div id = "container-El" class = "container todo-grid "></div>`
+    LoadPagePower();
 
-function updateinnerHTML() {
-    renderHTML(rows);
-    document.getElementById("container-El").innerHTML = str;
-    addEventListenerToAllDeleteButtons(rows);
-    localStorage.setItem('rows', JSON.stringify(rows))
+
+    function LoadPagePower() {
+        document.getElementById("add-el").addEventListener("click",  addTask );
+        var rows = JSON.parse(localStorage.getItem(`${user}rows`)) || [] ;
+        var str = "";
+        updateinnerHTML();
+    
+        function addEventListenerToAllDeleteButtons(arr){
+            for (let i = 0 ; i< arr.length ; i++){
+                document.getElementById(`delete-${i}th`).addEventListener("click", function DeleteAndUpdate() {
+                    arr.splice(i,1);               
+                    updateinnerHTML();
+                });
+            }
+        }
+    
+        function addTask() {
+            
+            var task = document.getElementById("task-input-el").value;
+            var date = document.getElementById("date-input-el").value;
+            rows.push([task,date]);
+            renderHTML(rows);
+            document.getElementById("container-El").innerHTML = str;   //these four lines maybe replaced
+            addEventListenerToAllDeleteButtons(rows);                  // with updateinnerHTML() function.
+            localStorage.setItem(`${user}rows`, JSON.stringify(rows))
+        }
+    
+        function updateinnerHTML() {
+            renderHTML(rows);
+            document.getElementById("container-El").innerHTML = str;
+            addEventListenerToAllDeleteButtons(rows);
+            localStorage.setItem(`${user}rows`, JSON.stringify(rows))
+            
+        }
+    
+        function renderHTML(arr) {
+            str = "";
+            for (var i = 0 ; i < arr.length ; i++){
+                task = arr[i][0];
+                date = arr[i][1];
+                str += `
+                            <div class="Lcolumn column">${task}</div>
+                            <div class="Mcolumn column">${date}</div>
+                            <button class="Rcolumn column" id = "delete-${i}th">Delete</button>
+                        `;
+            }
+        }
+    }
+    
+    
     
 }
 
-function renderHTML(arr) {
-    str = "";
-    for (var i = 0 ; i < arr.length ; i++){
-        task = arr[i][0];
-        date = arr[i][1];
-        str += `
-                    <div class="Lcolumn column">${task}</div>
-                    <div class="Mcolumn column">${date}</div>
-                    <button class="Rcolumn column" id = "delete-${i}th">Delete</button>
-                `;
-    }
-}
